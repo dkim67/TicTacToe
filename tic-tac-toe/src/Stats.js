f94a4ae5c06e260831e7c5244da144e04fe99b94
Daniel Kim <kim.daniel24@gmail.com>
2018-11-19 17:51:40 -0500

    play turn stats

---
+++ tic-tac-toe/src/Stats.js
@@ -0,0 +1,20 @@
+const Stats = props => {
+	const { squares, xIsNext } = props,
+		winner = calculateWinner(squares),
+		effect = winner?'bounce':''; 
+	let stats;  
+
+	if(winner) {
+		stats = 'Winner is: '+ winner;
+	}else{
+		stats = 'Next player is: '+ (xIsNext?'x':'o');
+	}
+	
+	
+	return(
+		<div className="game-info__status">
+			<div className={'stats '+effect}>{stats}</div>
+  		</div>
+	); 
+}
+
